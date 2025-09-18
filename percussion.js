630f1a12ba5cdaff1a60856fb7d650a088f7d68f
mankamaiie <lovemariie111@gmail.com>
2025-09-17 22:09:07 -0400

    percussion_patch_Mariia_Kovalenko

---
+++ percussion.js
@@ -0,0 +1,10 @@
+/* by Mariia K */
+
+setcpm(85/4)
+
+// first idea (commented out cause i don't really like it)
+// $: s("[bd,sd, - sd - [bd,sd]][sd, hh oh hh hh][hh [hh,bd][oh,bd] hh][sd,hh - hh hh]")
+//   .bank("rolandtr909")
+
+$: s("[bd,hh - - hh -] [sd,hh - - hh -] [bd,hh - - hh -] [sd,hh - - hh -]")
+  .bank("rolandtr909")
